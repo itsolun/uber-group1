558069e36394d82c26a06a1d6673a820e12b755d
Amr Hafez <amrhassan98@hotmail.com>
2020-09-03 03:08:05 +0200

    edit error search by username

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(express.json());
 
 const db = "uberDB";
 var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/' + db ;
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/' + db ;
 
 const mongoose = require("mongoose");
 mongoose.connect(mongoUri, { useNewUrlParser: true });
@@ -20,29 +20,29 @@ const carAPI   = require('./routes/car');
 const userAPI		= require('./routes/user');
 
 // models
-const Car   	= require('./models/car');
-const User		= require('./models/user');
-const Trip		= require('./models/trip');
-const Point		= require('./models/point');
+const Car = require('./models/car');
+const User = require('./models/user');
+const Trip = require('./models/trip');
+const Point = require('./models/point');
 
 router.route('/user')
 
-	.post(userAPI.postUser)
+.post(userAPI.postUser)
 
-	.get(userAPI.getAllUsers);
+.get(userAPI.getAllUsers);
 
 
 router.route('user/:_id')
 
-	.get(userAPI.getUserById)
+.get(userAPI.getUserById)
 
-	.put(userAPI.updateUser)
+.put(userAPI.updateUser)
 
-	.delete(userAPI.deleteUser);
+.delete(userAPI.deleteUser);
 
 
 app.use('/uber', router);
 
-const port     	= process.env.PORT || 3000
+const port = process.env.PORT || 3000
 app.listen(port);
 console.log('Starting UberLike on port ' + port);
